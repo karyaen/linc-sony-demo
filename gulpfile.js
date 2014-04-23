fc0a5aa6592be5965153519a5ff4074fc8a621c3
adamayres <magicaj@gmail.com>
2014-04-22 17:30:03 -0700

    feat(mobile): add mobile version of sony demo product page and some basic server side switching for mobile devices

--- gulpfile.js
+++ gulpfile.js
@@ -6,19 +6,29 @@ var nOpen = require('open');
 gulp.task('default', ['app']);
 
 gulp.task('app', function () {
-  var connect = require('connect');
-  var http = require('http');
+  var express = require('express');
+  var checkMobile = require('connect-mobile-detection');
+  var app = express();
 
-  var app = connect()
-    .use(connect.logger('dev'))
-    .use(connect.static('assets'))
-    .use(connect.static('assets/store.sony.com'))
-    .use(connect.static(
-    'assets/store.sony.com/55-class-54.6-diag-xbr-4k-ultra-hd-tv-zid27-XBR55X900A', {
-      index: 'cat-27-catid-Sony-HD-TVs.html'
-    }));
+  app.use(checkMobile());
 
-  http.createServer(app).listen(3000, function () {
+  app.use(express.static(__dirname + '/assets'));
+  app.use(express.static(__dirname + '/assets/store.sony.com'));
+  app.use(express.static(__dirname + '/assets/m'));
+  app.use(express.static(__dirname + '/assets/m/m.store.sony.com'));
+
+  app.get('/', function (req, res) {
+    var file;
+
+    if (req.mobile) {
+      file = 'assets/m/m.store.sony.com/products/27-XBR55X900A.html';
+    } else {
+      file = 'assets/store.sony.com/55-class-54.6-diag-xbr-4k-ultra-hd-tv-zid27-XBR55X900A/cat-27-catid-Sony-HD-TVs.html';
+    }
+    res.sendfile(file);
+  });
+
+  app.listen(3000, function () {
     nOpen('http://localhost:3000');
   });
 });
